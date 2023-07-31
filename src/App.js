829bfc975896e40f026e54ee64e8fc380f9757e8
Brian Guintos Recinos <brianguintos@gmail.com>
2023-07-31 15:38:21 -0700

    needs work

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
+import './App.css'
+
 import React, { useState } from 'react'
-import logo from './logo.svg';
-import './App.css';
-import ColorBlock from './ColorBlock';
+import ColorBlock from './ColorBlock'
+
 
 function App(){
     let [colors, setColors] = useState ([
         'violet', 'blue',
         'lightblue', 'green',
-        'greenYellow', 'yellow',
+        'greenyellow', 'yellow',
         'orange', 'red'
     ])
+    
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock color={color} />
+        )
+    })
 
     return (
-        <div className="App">
+    <div className="App">
+    {colors.map((color, i) => 
+        <ColorBlock key={i} color={color} />
+    )}
 
-        </div>
+    
+    )
+</div>
     )
 }
 
--- src/ColorBlock.js
+++ src/ColorBlock.js
@@ -1,43 +1,30 @@
-import React, { useState } from 'react'
-import ColorBlock from './ColorBlock'
+import React from 'react';
+
+function ColorBlock(props) {
+  return (
+    <div className="colorBlock" style={{ backgroundColor: props.color }}>
+      <p>{props.color}</p>
+    </div>
+  );
+}
 
 function App() {
-    let colors = [
-        'violet', 'blue',
-        'lightblue', 'green',
-        'greenyellow', 'yellow',
-        'orange', 'red'
-    ]
+  let colors = [
+    'violet', 'blue',
+    'lightblue', 'green',
+    'greenyellow', 'yellow',
+    'orange', 'red'
+  ];
 
-    let colorMap = colors.map((color, i) => {
-        return (
-            <ColorBlock color ={color} />
-        )
-    })
+  let colorMap = colors.map((color, i) => {
+    return <ColorBlock key={i} color={color} />;
+  });
 
-    return (
-        <div className="App">
-            {colorMap}
-        </div>
-    )
+  return (
+    <div className="App">
+      {colorMap}
+    </div>
+  );
 }
 
-<div className="App">
-    {colors.map((color, i) => 
-        <ColorBlock key={i} color={color} />
-    )}
-</div>
-
-
-function ColorBlock(props){
-    return (
-        <div className="colorBlock"
-        style={{'backgroundColor': props.color}}>
-            <p>{props.color}</p>
-        </div>
-    )
-}
-
-// Do not forget to export your component once you have built it!
 export default ColorBlock
- 
